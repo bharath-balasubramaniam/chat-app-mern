82817dd82712d66144ecaabda89406d9b7a507bb
Bharath Balasubramaniam <balasubramaniambharath7@gmail.com>
2021-11-24 14:24:54 +0530

    backend code complete

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 const { User } = require("./models/user");
+const { auth } = require("./middleware/auth");
 const config = require("./config/key");
 mongoose
   .connect(config.mongoURL, {
@@ -16,19 +17,53 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get("/user/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req.id,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+  });
+});
 app.post("/user/register", (req, res) => {
-  console.log("hii");
   const user = new User(req.body);
-  console.log(user);
   user.save((err, userData) => {
     if (err) return res.json({ success: false, err });
   });
   return res.status(200).json({ success: true });
 });
-
-app.get("/", (req, res) => {
-  res.send("hello World !!!");
+app.post("/user/login", (req, res) => {
+  //find  theemail
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed email not found",
+      });
+    }
+    //compare the password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "Wrong Password",
+        });
+      }
+    });
+    //generate token
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res.cookie("x_auth", user.token).status(200).json({ loginSuccess: true });
+    });
+  });
 });
-
+app.get("/user/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({ success: true });
+  });
+});
 const port = 5000;
 app.listen(port);
