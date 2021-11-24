f7961ad09f434b26659efcf841023f899b2ed074
Bharath Balasubramaniam <balasubramaniambharath7@gmail.com>
2021-11-24 12:24:02 +0530

    move private information into dev.js;

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+const { User } = require("./models/user");
+const config = require("./config/key");
 mongoose
-  .connect(
-    "mongodb+srv://bharath:!4Vishalini@chat-app-mern.1uidr.mongodb.net/chat-app-mern?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-    }
-  )
+  .connect(config.mongoURL, {
+    useNewUrlParser: true,
+  })
   .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post("/user/register", (req, res) => {
+  console.log("hii");
+  const user = new User(req.body);
+  console.log(user);
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+  });
+  return res.status(200).json({ success: true });
+});
+
 app.get("/", (req, res) => {
-  res.send("hello World !");
+  res.send("hello World !!!");
 });
 
 const port = 5000;
